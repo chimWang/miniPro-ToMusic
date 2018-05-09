18521d075c71e762cc26791426e925b8f6ceaa82
chimWang <chimWang1203@gmail.com>
2018-05-09 22:09:51 +0800

    miniProgram
    微信小程序

---
+++ pages/playMusic/playMusic.js
@@ -0,0 +1,86 @@
+// pages/playMusic/playMusic.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isPlayingMusic:false,
+    progress:0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.audioCtx=wx.createAudioContext('myAudio')
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  onMusicTap(){
+    if (this.data.isPlayingMusic){
+      this.audioCtx.pause()
+      this.setData({
+        isPlayingMusic: !this.data.isPlayingMusic
+      })
+    }else{
+      this.audioCtx.play()
+      this.setData({
+        isPlayingMusic: !this.data.isPlayingMusic
+      })
+    }
+  },
+  musicStart(e){
+    var progress = parseInt((e.detail.currentTime / e.detail.duration) * 100)
+    this.setData({
+      progress: progress
+    })
+  }
+})
