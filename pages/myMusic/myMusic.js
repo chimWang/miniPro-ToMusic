4958d3c8b5de90d22f6d2591c9a47f93abb91ad9
chimWang <chimWang1203@gmail.com>
2018-05-11 22:57:45 +0800

    微信小程序
    微信小程序

--- pages/myMusic/myMusic.js
+++ pages/myMusic/myMusic.js
@@ -1,27 +1,34 @@
 // pages/myMusic/myMusic.js
+var touchDot = 0;
+var nth = 0;
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    musicList:[
+    left: 0,
+    mainBgImg: '/images/index/welcome.jpg',
+    musicList: [
       {
-        name:'Jungle Pink',
-        date:'2018-5-10',
-        bgImg:'/images/playMusic/background.jpg'
-      },
-      {
-        name: '55555',
+        selected: false,
+        name: 'Jungle Pink',
         date: '2018-5-10',
         bgImg: '/images/playMusic/background.jpg'
       },
       {
+        selected: true,
+        name: '55555',
+        date: '2018-5-10',
+        bgImg: '/images/index/welcome.jpg'
+      },
+      {
+        selected: false,
         name: '2222',
         date: '2018-5-10',
         bgImg: '/images/playMusic/background.jpg'
       },
       {
+        selected: false,
         name: '2222',
         date: '2018-5-10',
         bgImg: '/images/playMusic/background.jpg'
@@ -33,68 +40,106 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
-  back(){
+  back() {
     wx.navigateBack({
-      
+
     })
   },
-  listen(){
+  listen() {
     wx.navigateTo({
       url: '../playMusic/playMusic',
     })
   },
-  musicScroll(e){
-    console.log(e.detail.scrollLeft)
+  // musicScroll(e){
+  //   var itemWidth=e.detail.scrollWidth / this.data.musicList.length;
+  //   var scrollLeft=e.detail.scrollLeft;
+  //   var curIndex = Math.round(scrollLeft / itemWidth);
+  //   for (var i = 0,len = this.data.musicList.length;i<len;i++){
+  //     this.data.musicList[i].selected=false
+  //   }
+  //   this.data.musicList[curIndex].selected=true
+  //   this.data.mainBgImg = this.data.musicList[curIndex].bgImg
+  //   this.setData({
+  //     musicList: this.data.musicList,
+  //     mainBgImg: this.data.mainBgImg
+  //   })
+  // },
+  touchStart(e) {
+    touchDot = e.touches[0].pageX;
+    console.log(touchDot + '----------')
+  },
+  touchMove(e) {
+    let touchMove = e.touches[0].pageX;
+    let diff = touchMove - touchDot
+    console.log(diff+'..........')
+    if (diff <= -60) {
+      this.data.left -= 250
+      this.setData({
+        left:this.data.left
+      })
+    }
+  },
+  swiperChange(e){
+    let nowIndex = e.detail.current
+    this.data.musicList.forEach((item)=>{
+      item.selected=false;
+    })
+    this.data.musicList[nowIndex].selected=true
+    this.setData({
+      musicList: this.data.musicList,
+      mainBgImg: this.data.musicList[nowIndex].bgImg
+    })
   }
+
 })
